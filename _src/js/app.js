a233a0b16edabd56aeb6b2e371c43225b5a2130e
Rob Easthope <robeasthope@gmail.com>
2013-08-23 15:00:47 +0100

    Landing page fix

--- _src/js/app.js
+++ _src/js/app.js
@@ -4,18 +4,18 @@ function oknFullScreen() {
 	if ($(window).height() > $(".bkg").height() + 50) {
 		// Enlarge jumbotron to fullscreen
 		$(".bkg").css("height",function(){
-			return ($(window).height());
+			return ($(window).height() - 50);
 		});
 	}
 }
 
 function oknFullScreenLandingText() {
-	if ($(".bkg").height() < $(window).height()) {
+	//if ($(".landing-wrap").height() > $(window).height()) {
 		// Enlarge jumbotron to fullscreen
 		$(".landing-wrap").css("height",function(){
-			return ($("window").height());
+			return ($(".bkg").height() - 90);
 		});
-	}
+	//}
 }
 
 $(document).ready(function() {
