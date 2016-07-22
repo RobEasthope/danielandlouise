276c5a8ba8f97cbdf66da55d635b73a36aaf85d6
Rob Easthope <robeasthope@gmail.com>
2016-07-22 15:35:50 +0100

    Add gulp code for asset revisioning

--- gulpfile.js
+++ gulpfile.js
@@ -70,6 +70,9 @@ gulp.task('html', ['jade', 'styles', 'scripts'], () => {
     .pipe($.useref({searchPath: ['.tmp', 'app', '.']}))
     .pipe($.if('*.js', $.uglify()))
     .pipe($.if('*.css', $.cssnano({safe: true, autoprefixer: false})))
+		.pipe($.if('*.js', $.rev()))
+		.pipe($.if('*.css', $.rev()))
+		.pipe($.revReplace())
     .pipe($.if('*.html', $.htmlmin({collapseWhitespace: true})))
     .pipe(gulp.dest('dist'));
 });
